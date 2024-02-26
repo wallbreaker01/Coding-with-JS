ced5257465525d4b83a7b442c6277d806260d506
wallbreaker01 <srshowmik2001@gmail.com>
2024-02-26 14:03:39 +0600

    commmit

---
+++ day-5/script.js
@@ -0,0 +1,13 @@
+const panels = document.querySelectorAll('.panel');
+// console.log(panels); 
+
+function toggleOpen() {
+    this.classList.toggle('open');
+}
+function textActive(e) {
+    if (e.propertyName.includes('flex')) {
+        this.classList.toggle('open-active');
+    }
+}
+panels.forEach((panel) => panel.addEventListener('click', toggleOpen));
+panels.forEach(panel => panel.addEventListener('transitionend', textActive));
